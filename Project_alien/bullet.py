c4bdfe406e15fd924df6950df6ce5ee7b5e78c30
ksmit117 <158485702+ksmit117@users.noreply.github.com>
2024-04-18 16:17:38 -0400

    Add files via upload

---
+++ Project_alien/bullet.py
@@ -0,0 +1,30 @@
+import pygame
+from pygame.sprite import Sprite
+
+class Bullet(Sprite):
+    """A class to mange bullets fired from the ship"""
+    def __init__(self, ai_game):
+        """create a bullet fired from the ship"""
+        super().__init__()
+        self.screen = ai_game.screen
+        self.settings = ai_game.settings 
+        self.color = self.settings.bullet_color
+
+        #Create a bullet rect at (0,0) and then set correct position
+        self.rect = pygame.Rect(0, 0, self.settings.bullet_height, self.settings.bullet_width)
+        self.rect.midleft = ai_game.ship.rect.midleft
+        
+        #store the bullet's position as a float
+        self.y = float(self.rect.y)
+        self.x = float(self.rect.x)
+    
+    def update(self):
+        """Move the bullet up the screen"""
+        #update the exact position of the bullet
+        self.x -= self.settings.bullet_speed
+        # update the rect position
+        self.rect.x = self.x
+        
+    def draw_bullet(self):
+        """Draw the bullet to the screen"""
+        pygame.draw.rect(self.screen, self.color, self.rect)
---
+++ Project_alien/ship.py
@@ -0,0 +1,49 @@
+import pygame
+
+class Ship:
+    """A class to mange the ship"""
+    
+    def __init__(self, ai_game):
+        """initialize the ship and set its varitent"""
+        self.screen = ai_game.screen
+        self.settings = ai_game.settings
+        self.screen_rect = ai_game.screen.get_rect()
+        
+        #Load the ship image and get its rect.
+        self.image = pygame.image.load('armored_skeleton_slash.gif')
+        self.rect = self.image.get_rect()
+        #start each new ship at the bottom center of the screen
+        self.rect.center = self.screen_rect.center
+        
+        #Store a float for the ship's exact horizontal position
+        self.x = float(self.rect.x)
+        
+        
+        #Movement flag; start with a ship that's not moving
+        self.moving_right = False
+        self.moving_left = False
+        self.moving_up = False
+        self.moving_down = False
+        
+        
+    def update(self):
+        """update the ship's position based on the movement flag"""
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+        if self.moving_up and self.rect.top > 0:
+            self.rect.y -= self.settings.ship_speed
+        if self.moving_down and self.rect.bottom < self.screen_rect.bottom:
+            self.rect.y += self.settings.ship_speed
+
+        
+            
+        #update rect object from self.x
+        self.rect.x = self.x
+        
+    def blitme(self):
+        """Draw the ship at its current location"""
+        rotated_image = pygame.transform.rotate(self.image, 90)
+        rotated_rect = rotated_image.get_rect(center=self.rect.center)
+        self.screen.blit(rotated_image, rotated_rect)
